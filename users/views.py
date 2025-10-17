00e1f61ae2bf9005fdd5733e47f9bdf2b04d741f
zeyneb Mulat <zeynebmu40@gmail.com>
2025-10-17 14:35:18 +0300

    second commit

--- users/views.py
+++ users/views.py
@@ -3,7 +3,7 @@ from django.contrib.auth.decorators import login_required
 
 @login_required
 def profile_view(request):
-    return render(request, 'users/profile.html', {'user': request.user})
+    return render(request, 'users/profile.html', {'user': request.user, 'name': 'Zeyneb'})
 
 def about(request):
     return render(request,'blog/about.html',{'title':'About Page'}) 
