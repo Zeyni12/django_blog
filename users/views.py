2457a3847643dbf4704d73b7a6b54802177cd7bb
zeyni12 <zeynebmu40@gmail.com>
2025-10-30 12:52:39 +0300

    forms

--- users/views.py
+++ users/views.py
@@ -14,6 +14,7 @@ from django.contrib.auth import login, authenticate
 from django.contrib.auth.forms import UserCreationForm
 from django.contrib.auth.views import LoginView, LogoutView
 from .forms import UserRegisterForm , UserUpdateForm,ProfileUpdateForm 
+from django.contrib import messages
 
 # ✅ Register view
 def register(request):
@@ -30,8 +31,18 @@ def register(request):
 # ✅ Profile view
 @login_required
 def profile(request):
-    u_form = UserUpdateForm()
-    p_form = ProfileUpdateForm()
+    if request.method == 'POST':
+        u_form = UserUpdateForm(request.POST, instance=request.user)
+        p_form = ProfileUpdateForm(request.POST, request.FILES, instance=request.user.profile)
+        if u_form.is_valid() and p_form.is_valid():
+            u_form.save()
+            p_form.save()
+            messages.success(request, f'Your Profile was Updated!!')
+            return redirect('profile')
+    else:
+        u_form = UserUpdateForm(instance=request.user)
+        p_form = ProfileUpdateForm(instance=request.user.profile)
+            
     context = {
         'u_form': u_form,
         'p_form': p_form
